42795a420dbfeb574cb912c47ac3776c14dbfe42
Svmpankaj <svmpankaj@gmail.com>
2023-05-12 23:03:00 +0530

    card shuffle with number is created

---
+++ deck.js
@@ -0,0 +1,46 @@
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+]
+
+export default class Deck {
+    constructor(cards = freshDeck()) {
+        this.cards = cards;
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const newIndex = Math.floor(Math.random() * (i + 1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oninvalid
+        }
+    }
+}
+
+class Card {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
+
+function freshDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
+}
