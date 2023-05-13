2506d6ba270e2c5a99c59336e7a29ef49cae6bba
Svmpankaj <svmpankaj@gmail.com>
2023-05-13 21:10:56 +0530

    card game is finished

--- deck.js
+++ deck.js
@@ -20,12 +20,24 @@ export default class Deck {
         this.cards = cards;
     }
 
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    pop() {
+        return this.cards.shift()
+    }
+
+    push(card) {
+        this.cards.push(card)
+    }
+
     shuffle() {
-        for (let i = this.cards.length - 1; i > 0; i--) {
+        for (let i = this.numberOfCards - 1; i > 0; i--) {
             const newIndex = Math.floor(Math.random() * (i + 1))
             const oldValue = this.cards[newIndex]
             this.cards[newIndex] = this.cards[i]
-            this.cards[i] = oninvalid
+            this.cards[i] = oldValue
         }
     }
 }
@@ -34,6 +46,18 @@ class Card {
     constructor(suit, value) {
         this.suit = suit;
         this.value = value;
+    }
+
+    get color() {
+        return this.suit === '♣' || this.suit === '♠' ? 'black' : 'red'
+    }
+
+    getHTML() {
+        const cardDiv = document.createElement('div')
+        cardDiv.innerText = this.suit
+        cardDiv.classList.add("card", this.color)
+        cardDiv.dataset.value = `${this.value} ${this.suit}`
+        return cardDiv
     }
 }
 
